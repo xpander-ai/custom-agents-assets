ae42625b038ce3cc74b8a1cf3d0a81a25815e60e
Moriel Pahima <moriel@xpander.ai>
2025-08-06 13:06:51 +0300

    feat(PRO-67): migrate to agno

--- agno-template/xpander_handler.py
+++ agno-template/xpander_handler.py
@@ -7,7 +7,7 @@ from agno.agent import Agent
 
 @on_task
 async def my_agent_handler(task: Task):
-    backend = Backend()
+    backend = Backend(configuration=task.configuration)
     agno_args = await backend.aget_args(task=task)
     agno_agent = Agent(**agno_args)
     result = await agno_agent.arun(message=task.to_message())
