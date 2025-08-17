5a3c2adfcc02ffe0749f8a0016e5d93f9ebb0fd9
Moriel Pahima <moriel@xpander.ai>
2025-08-17 14:41:41 +0300

    updated template

--- agno-template-team/xpander_handler.py
+++ agno-template-team/xpander_handler.py
@@ -2,7 +2,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-from xpander_sdk import Task, Backend, on_task
+from xpander_sdk import Task, Backend, on_task, OutputFormat
+from pydantic import BaseModel
 from agno.team import Team
 
 @on_task
@@ -11,5 +12,12 @@ async def my_agent_handler(task: Task):
     agno_args = await backend.aget_args(task=task)
     agno_agent = Team(**agno_args)
     result = await agno_agent.arun(message=task.to_message())
+    
+    # in case of structured output, return as stringified json
+    if task.output_format == OutputFormat.Json and isinstance(result.content, BaseModel):
+        task.result = result.content.model_dump_json()
+    else:
+        task.result = result.content
+    
     task.result = result.content
     return task
--- agno-template/xpander_handler.py
+++ agno-template/xpander_handler.py
@@ -2,7 +2,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-from xpander_sdk import Task, Backend, on_task
+from xpander_sdk import Task, Backend, on_task, OutputFormat
+from pydantic import BaseModel
 from agno.agent import Agent
 
 @on_task
@@ -11,5 +12,10 @@ async def my_agent_handler(task: Task):
     agno_args = await backend.aget_args(task=task)
     agno_agent = Agent(**agno_args)
     result = await agno_agent.arun(message=task.to_message())
-    task.result = result.content
+    
+    # in case of structured output, return as stringified json
+    if task.output_format == OutputFormat.Json and isinstance(result.content, BaseModel):
+        task.result = result.content.model_dump_json()
+    else:
+        task.result = result.content
     return task
